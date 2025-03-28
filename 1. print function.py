62d6914187a1ddcd14ba63e1d43a1c9922db3bda
Tanisha0409 <124240466+Tanisha0409@users.noreply.github.com>
2025-03-28 22:15:30 +0530

    Add files via upload

---
+++ 1. print function.py
@@ -0,0 +1,25 @@
+print("hello world")
+
+print('day-1 python print function')
+
+print("the function is declared like this: ")
+
+
+#print("print("what to print")")
+# error because what to print is a kind of code and not the string
+
+
+#\n will change the line
+print("hello world!\nhello world!")
+
+#string concat
+print("hello"+"angella")
+print("hello "+"angella")
+print("hello"+" angella")
+print("hello"+" "+"angella")
+
+
+
+#indentation error
+#  print("hello tanisha")
+
