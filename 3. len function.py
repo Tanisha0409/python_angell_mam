5eccf20ea876e56616bb5ac7f4dd25d7e1e0c43d
Tanisha0409 <124240466+Tanisha0409@users.noreply.github.com>
2025-03-28 22:18:28 +0530

    Add files via upload

---
+++ 3. len function.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 28 22:16:09 2025
+
+@author: Dell
+"""
+
+# len function
+
+print(len("jack"))
+
+print(len("angella"))
+
+print(len(input("what is your name? ")))
