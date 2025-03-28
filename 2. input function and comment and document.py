b9f4ad73936c1c48fff391451263177aa77b1779
Tanisha0409 <124240466+Tanisha0409@users.noreply.github.com>
2025-03-28 22:15:52 +0530

    Add files via upload

---
+++ 2. input function and comment and document.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 28 22:08:41 2025
+
+@author: Dell
+"""
+
+#input function
+
+print("hello " + input("what is your name: "))
+
+print("hello "+ input("what is your name? ") + "!")
+
+#comment and doc string
+
+#input user name() - comment
+
+'''
+this is my documentary
+
+'''
+
+"""
+this is my second documentary
+
+"""
