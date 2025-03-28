0d62ecb38d9c39138f2e3a5e4d9ac03ae9ac1994
Tanisha0409 <124240466+Tanisha0409@users.noreply.github.com>
2025-03-28 22:29:32 +0530

    Add files via upload

---
+++ 4. variable and swap 2 no..py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 28 22:18:35 2025
+
+@author: Dell
+"""
+
+#python variable
+
+name = input("what is your name? ")
+print(name)
+
+
+name = "jack"
+print(name)
+
+#this will give name error 
+#name = "angella"
+#print(nama)
+
+name1 = input("what is your name? ")
+l = len(name1)
+print(l)
+
+
+#swap to number using comma
+a = 24
+b = 29
+
+a,b = b,a
+
+print(a,b)
+
+#swap 2 no. using 3rd variable
+a = 9
+b = 4
+c = 0
+
+c = a
+a = b
+b = c
+
+print(a , b)
