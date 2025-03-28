c0c8ce95da935bab914c0ffec3d3fe0479349f21
Tanisha0409 <124240466+Tanisha0409@users.noreply.github.com>
2025-03-28 22:57:38 +0530

    Add files via upload

---
+++ 7. project - sum of two digit no..py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 28 22:52:34 2025
+
+@author: Dell
+"""
+
+#project2 - input=39 , output=3+9 =12
+
+two_digit_no = input("type a two digit no: ")
+
+first_digit = two_digit_no[0]
+second_digit = two_digit_no[1]
+
+result = int(first_digit) +int(second_digit)
+print(result)
